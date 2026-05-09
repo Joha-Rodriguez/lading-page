6505fded2b094b9b7b4cdec1290e0059f41e5777
Joha-Rodriguez <jmrodriguezg@ut.edu.co>
2026-05-09 15:04:18 -0500

    Update vite config base for GH Pages and rebuild

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/lading-page/',
   plugins: [react()],
   resolve: {
     alias: {
